03e15112408115ec6688a442732630c5aaf3e71a
shecannotsee <shecannotsee@163.com>
2023-09-02 15:51:47 +0800

    [feat] 1.添加业务接口示例

---
+++ src/services.cpp
@@ -0,0 +1,13 @@
+//
+// Created by shecannotsee on 23-9-2.
+//
+
+#include "services.h"
+
+void services::interface_route_N1(Response response, Request request) {
+
+}
+
+void services::interface_route_01(Response response, Request request) {
+
+}
---
+++ src/services.h
@@ -0,0 +1,22 @@
+//
+// Created by shecannotsee on 23-9-2.
+//
+
+#ifndef HTTP_MOCK_SERVER_SERVICES_H
+#define HTTP_MOCK_SERVER_SERVICES_H
+
+#include <server_http.hpp>
+
+class services {
+ public:
+  using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+  using Response   = std::shared_ptr<HttpServer::Response>;
+  using Request    = std::shared_ptr<HttpServer::Request>;
+ public:
+  void interface_route_N1(Response,Request);
+  void interface_route_01(Response,Request);
+
+};
+
+
+#endif //HTTP_MOCK_SERVER_SERVICES_H
